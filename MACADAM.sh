2fabe8b8d53286aaab8be4c75929c9aec2816ece
malo9@free.fr <a@a.fr>
2019-02-06 11:41:39 +0100

    Correcting Archaea

--- MACADAM.sh
+++ MACADAM.sh
@@ -6,13 +6,13 @@ bash ./Bacteria/DLRefSeq.sh
 
 rm -rf /work/mleboulch/PathT/ptools-local/pgdbs/user/
 
-bash ./Arche/DLRefSeq.sh
+bash ./Archaea/DLRefSeq.sh
 
 rm -rf /work/mleboulch/PathT/ptools-local/pgdbs/user/
 
-cp -rf ./Arche/DatabaseTSV/* MergedFile/Arche/
+cp -rf ./Archaea/DatabaseTSV/* MergedFile/Archaea/
 
 cp -rf Bacteria/DatabaseTSV/* MergedFile/Bact/
 
-python3 MergedFile/MergedArcheBact.py
+python3 MergedFile/MergedArchaeaBact.py
 python3 MergedFile/ImportSQLite.py
